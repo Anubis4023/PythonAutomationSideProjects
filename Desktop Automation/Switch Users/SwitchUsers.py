9f0c64db5d8367435882b1640b922ee09f0b2a3a
anubis4023 <franciscco.maldonad.601@gmail.com>
2022-05-09 19:00:04 -0700

    Fix comment

--- Desktop Automation/Switch Users/SwitchUsers.py
+++ Desktop Automation/Switch Users/SwitchUsers.py
@@ -1,7 +1,7 @@
 from pywinauto.application import Application
 import mouse
 import time
-""""
+
 #Open the mouse settings application and connect to the window that opens
 openMouse = Application(backend="uia").start('C:\Program Files (x86)\Glorious Model O Software\OemDrv.exe')#.connect(title='Glorious Model O Software', timeout=3)
 #openMouse.GloriousModelOSoftware.print_control_identifiers()
@@ -19,7 +19,7 @@ mouse.click(button='left')
 mouse.move(1426, 227, True, 0)
 time.sleep(1)
 mouse.click(button='left')
-"""
+
 
 #Open discord and connect to the window that opens
 discord = Application(backend="uia").start(r"C:\Users\pacow\AppData\Local\DiscordPTB\app-1.0.1013\DiscordPTB.exe")#.connect(title = 'Discord', timeout=10)
