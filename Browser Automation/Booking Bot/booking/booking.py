a6e7f3a2d4d407128eb2c10737d6917405360f2e
anubis4023 <franciscco.maldonad.601@gmail.com>
2022-05-12 13:48:11 -0700

    Added choice of currency

--- Browser Automation/Booking Bot/booking/booking.py
+++ Browser Automation/Booking Bot/booking/booking.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import os
 import booking.constants as const
 
 
 class Booking(webdriver.Chrome):
-    def __init__(self):
+    def __init__(self, teardown=False):
+        self.teardown = teardown
         super(Booking, self).__init__()
         self.implicitly_wait(15) #Find element methods will wait x seconds
         self.maximize_window()
@@ -13,5 +15,12 @@ class Booking(webdriver.Chrome):
         self.get(const.BASE_URL)
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
-        self.quit()
-    
+        if self.teardown:
+            self.quit()
+
+    def select_currency(self, currency=None):
+        currency_button = self.find_element(By.CSS_SELECTOR, 'button[data-tooltip-text="Choose your currency"]')
+        currency_button.click()
+
+        select_currency = self.find_element(By.CSS_SELECTOR, f'a[data-modal-header-async-url-param*="selected_currency={currency}"]')
+        select_currency.click()
--- Browser Automation/Booking Bot/main.py
+++ Browser Automation/Booking Bot/main.py
@@ -2,4 +2,5 @@ from booking.booking import Booking
 
 with Booking() as bot:
     bot.land_first_page()
+    #bot.select_currency(currency='MXN')
     print("Exiting...")
