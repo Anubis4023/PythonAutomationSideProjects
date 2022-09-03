3bbedcc4a5f230593a1e8f0080530b4d688941d4
Anubis4023 <franciscco.maldonad.601@gmail.com>
2022-09-02 20:45:46 -0700

    Set up for unit testing

--- Browser Automation/Paycheck/main.py
+++ Browser Automation/Paycheck/main.py
@@ -1,58 +1,10 @@
-from datetime import datetime
-
-def clock_hours():
-    #Get the date for the day worked
-    format = "%m/%d/%Y"
-    while True:
-        try:
-            date = input("Enter the date (mm/dd): ")
-            date += "/2022"
-            datetime.strptime(date, format)
-            #print("The string is a date with format " + format)
-            break
-        except ValueError:
-            print("The date does not have the correct format or is not a valid date")
-
-    hours = input("Enter number of hours worked: ")
-
-    log = open("log.txt", "r")
-
-    #Read the file log and store it as a list of lines in data (data[0] is the first line, total money owed)
-    data = log.readlines()
-
-    #Update the log with date and hours worked
-    data.append(date + ": " + hours + " hours worked\n")
-    total = int(data[0]) + int(hours) * 15
-    data[0] = str(total) + "\n"
-    print("New total:", total, ". Total increase is $", int(hours) * 15)
-    log.close()
-    #Update the total amount owed and the log dates
-    log = open("log.txt", "w")
-    #print(data)
-    log.writelines(data)
-    log.close()
-
-def add_pay():
-    pay = input("How much has been paid?\n")
-    log = open("log.txt", "r")
-    #Read the file log and store it in data as a list of lines
-    data = log.readlines()
-    #Update the log with date payed and how much is left owed
-    owed = str(int(data[0]) - int(pay))
-    data.append('$' + pay + " paid. Old total: " + data[0].strip() + ". New total: " + owed + "\n") #Update log with a log of the payment
-    data[0] = owed + "\n" #Update the money still left owed
-    log.close()
-    #Update the total amount at the top of the log
-    log = open("log.txt", "w")
-    log.writelines(data)
-    log.close()
-    print('$' + pay + " has been paid")
+import logging
 
 option = int(input("Option 1: Clock hours\nOption 2: Add payment\n"))
 if option == 1:
-    clock_hours()
+    logging.clock_hours()
 elif option == 2:
-    add_pay()
+    logging.add_pay()
 else:
     print("Error: Choose an available option")
 
---
+++ Browser Automation/Paycheck/test_logging.py
@@ -0,0 +1,13 @@
+import unittest
+import logging
+
+class TestLogging(unittest.TestCase):
+    def test_Hours(self):
+        
+        assert True
+
+    def test_Pay(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
