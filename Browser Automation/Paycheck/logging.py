71a2a7340268963f6bc2ab83223299a6c92633ba
Anubis4023 <franciscco.maldonad.601@gmail.com>
2022-09-03 16:09:20 -0700

    Added unit tests for clock_hours and add_pay functions

--- Browser Automation/Paycheck/logging.py
+++ Browser Automation/Paycheck/logging.py
@@ -1,40 +1,24 @@
-from datetime import datetime
 
-def clock_hours():
-    #Get the date for the day worked
-    format = "%m/%d/%Y"
-    while True:
-        try:
-            date = input("Enter the date (mm/dd): ")
-            date += "/2022"
-            datetime.strptime(date, format)
-            #print("The string is a date with format " + format)
-            break
-        except ValueError:
-            print("The date does not have the correct format or is not a valid date")
-
-    hours = input("Enter number of hours worked: ")
-
-    log = open("log.txt", "r")
+def clock_hours(date, hours, hourlyPay, fileName):
+    log = open(fileName, "r")
 
     #Read the file log and store it as a list of lines in data (data[0] is the first line, total money owed)
     data = log.readlines()
 
     #Update the log with date and hours worked
     data.append(date + ": " + hours + " hours worked\n")
-    total = int(data[0]) + int(hours) * 15
+    total = int(data[0]) + int(hours) * hourlyPay
     data[0] = str(total) + "\n"
-    print("New total:", total, ". Total increase is $", int(hours) * 15)
+    print("New total:", total, ". Total increase is $", int(hours) * hourlyPay)
     log.close()
     #Update the total amount owed and the log dates
-    log = open("log.txt", "w")
+    log = open(fileName, "w")
     #print(data)
     log.writelines(data)
     log.close()
 
-def add_pay():
-    pay = input("How much has been paid?\n")
-    log = open("log.txt", "r")
+def add_pay(pay, fileName):
+    log = open(fileName, "r")
     #Read the file log and store it in data as a list of lines
     data = log.readlines()
     #Update the log with date payed and how much is left owed
@@ -43,7 +27,7 @@ def add_pay():
     data[0] = owed + "\n" #Update the money still left owed
     log.close()
     #Update the total amount at the top of the log
-    log = open("log.txt", "w")
+    log = open(fileName, "w")
     log.writelines(data)
     log.close()
     print('$' + pay + " has been paid")
--- Browser Automation/Paycheck/test_logging.py
+++ Browser Automation/Paycheck/test_logging.py
@@ -3,11 +3,70 @@ import logging
 
 class TestLogging(unittest.TestCase):
     def test_Hours(self):
-        
-        assert True
+        hourlyPay = 15
+        log = open("test.txt", "r")
+        oldData = log.readlines()
+        log.close()
+
+        logging.clock_hours("3/5/2022", "6", hourlyPay, "test.txt")
+
+        log = open("test.txt", "r")
+        data = log.readlines()
+        updatedPay = int(data[0])
+        newestLog = data[-1]
+        log.close()
+
+        self.assertEqual(updatedPay, 144)
+        self.assertEqual(newestLog, "3/5/2022: 6 hours worked\n")
+
+        logging.clock_hours("3/6/2022", "10", hourlyPay, "test.txt")
+        logging.clock_hours("3/7/2022", "2", hourlyPay, "test.txt")
+        logging.clock_hours("3/10/2022", "4", hourlyPay, "test.txt")
+
+        log = open("test.txt", "r")
+        data = log.readlines()
+        updatedPay = int(data[0])
+        logOne = data[-3]
+        logTwo = data[-2]
+        logThree = data[-1]
+        log.close()
+
+        self.assertEqual(updatedPay, 384)
+        self.assertEqual(logOne, "3/6/2022: 10 hours worked\n")
+        self.assertEqual(logTwo, "3/7/2022: 2 hours worked\n")
+        self.assertEqual(logThree, "3/10/2022: 4 hours worked\n")
+
+        log = open("test.txt", "w")
+        log.writelines(oldData)
+        #print(oldData)
+        log.close()
 
     def test_Pay(self):
-        pass
+        log = open("test.txt", "r")
+        oldData = log.readlines()
+        log.close()
+
+        logging.add_pay("34", "test.txt")
+        
+        log = open("test.txt", "r")
+        data = log.readlines()
+        log.close()
+
+        newOwed = data[0]
+        self.assertEqual(newOwed, "20\n")
+        
+        i = 1
+        for logInstance in data[1:-1]:
+            self.assertEqual(logInstance, oldData[i])
+            i += 1
+
+        lastLog = data[-1]
+        self.assertEqual(lastLog, "$34 paid. Old total: 54. New total: 20\n")
+
+        log = open("test.txt", "w")
+        log.writelines(oldData)
+        log.close()
+        
 
 if __name__ == '__main__':
     unittest.main()
